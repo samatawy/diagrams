0b926e61afdd4437984ebfc1f7b721a41410e0fa
Sam Atawy <sam.atawy@gmail.com>
2026-07-21 19:14:30 +0300

    Bug fix in build pipeline

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,9 +3,17 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
     entry: ['src/index.ts'],
-    format: ['esm', 'cjs'],
+    format: ['esm', 'cjs', 'iife'],
+    platform: 'browser',
+    bundle: true,
+    sourcemap: true,
+    splitting: false,
+    dts: true,
     external: [
         'file-type',
         'probe-image-size'
+    ],
+    noExternal: [
+        '@samatawy/checks'
     ]
 });
